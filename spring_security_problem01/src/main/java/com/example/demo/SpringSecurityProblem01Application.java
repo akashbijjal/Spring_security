39404e6667dc5f9bf54e61c5665a50c102bd54bf
akashnbijjal <akash.bijjal@gmail.com>
2022-07-11 11:29:03 +0530

    spring security 01

---
+++ spring_security_problem01/src/main/java/com/example/demo/SpringSecurityProblem01Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringSecurityProblem01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityProblem01Application.class, args);
+	}
+
+}
